cc9947b3cb866da20d494724e844514b1d141820
Iykekelvins <kelvinochubili@gmail.com>
2021-12-05 19:51:18 +0100

    icons and images imports

---
+++ public/appIcons.ts
@@ -0,0 +1,25 @@
+import beauty from "../assets/beauty.png";
+import calendly from "../assets/calendly.png";
+import instagram from "../assets/instagram.png";
+import link from "../assets/link.png";
+import mailchimp from "../assets/mailchimp.png";
+import matte from "../assets/matte.png";
+import music from "../assets/music.png";
+import paypal from "../assets/paypal.png";
+import spotify from "../assets/spotify.png";
+import tiktok from "../assets/tiktok.png";
+import youtube from "../assets/youtube.png";
+
+export const appIcons = {
+  beauty,
+  calendly,
+  instagram,
+  link,
+  mailchimp,
+  matte,
+  music,
+  paypal,
+  spotify,
+  tiktok,
+  youtube,
+};
---
+++ public/appImages.ts
@@ -0,0 +1,23 @@
+import dashboard1 from "../assets/dashboard_pic1.png";
+import dashboard2 from "../assets/dashboard_pic2.png";
+import explore from "../assets/explore.png";
+import iphone from "../assets/iphone.png";
+import model from "../assets/model.png";
+import payment1 from "../assets/payment1.png";
+import payment2 from "../assets/payment2.png";
+import person1 from "../assets/person1.png";
+import person2 from "../assets/person2.png";
+import person3 from "../assets/person3.png";
+
+export const appImages = {
+  dashboard1,
+  dashboard2,
+  explore,
+  iphone,
+  model,
+  payment1,
+  payment2,
+  person1,
+  person2,
+  person3,
+};
